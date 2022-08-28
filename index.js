b7708531f9c91dc65920570679a7e2a73f306cc8
ABU TAHER <abutaher267@gmail.com>
2022-08-28 15:00:58 +0600

    messages marge

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
+// const fs = require("fs");
 const data1 = require("./data1.json");
 const data2 = require("./data2.json");
-const dataArrays = [ data1, data2];
-const dataArrays2 = [ data2, data1];
-const fs = require("fs");
-let test = false;
-// console.log(dataArrays);
-// const newArrs = [];
+const dataArrays2 = [ data2.data, data1.data];
 
-const tracker = [];
+const objsOfObj={}
 
-const stracuteChange = dataArrays2.reduce((acc, cur, idx) => {
-  let newAcc = {};
-  for (const id of Object.keys(cur.data)) {
-    if (tracker.indexOf(id) === -1) {
-      tracker.push(id);
-      tracker.push(idx);
-      // console.log(id)
-      newAcc = { ...cur, data: { ...cur.data, [id]: cur.data[id] } };
-    } else {
-      const checkIndex = tracker[tracker.indexOf(id) + 1];
-
-      // console.log(id);
-      const anotherAcc = [...acc];
-
-      anotherAcc[checkIndex] = {
-        ...anotherAcc[checkIndex],
-        data: {
-          ...anotherAcc[checkIndex].data,
-          [id]: {
-            ...anotherAcc[checkIndex].data[id],
-            messages: anotherAcc[checkIndex].data[id].messages.concat(
-              cur.data[id].messages
-            ),
-          },
-          // [anotherAcc[checkIndex].data[id]]:
-        },
-        // anotherAcc[checkIndex].data[id].messages=
-      }; //message concat
-      acc = anotherAcc;
-    } //else
+for (const item of dataArrays2) {
+  for (const id of Object.keys(item)) {
+    if(objsOfObj[id]===undefined){
+ 
+      objsOfObj[id]=item[id]
+    }else{
+      objsOfObj[id]={...objsOfObj[id], messages:[...objsOfObj[id].messages, ...item[id].messages]}
+    }
   }
-  acc = [...acc, newAcc];
-  return acc;
-}, []);
+}
 
-// console.log("=", stracuteChange);
-// if (test === false) {
-//   console.log(test);
+// console.log(objsOfObj)
 
 // const write = ()=>fs.writeFile(
 //   `books${Math.random().toString()}.json`,
-//   JSON.stringify(stracuteChange),
+//   JSON.stringify(objsOfObj),
 //   (err) => {}
 // );
 // setTimeout(()=>write(), 2000)
